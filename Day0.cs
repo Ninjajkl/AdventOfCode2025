3318ce30190d0eb921900d49976ab9f3840549a1
Joey Parker <Joseph.Parker@Charter.com>
2025-12-01 15:57:29 -0500

    feat: added Day 1

--- Day0.cs
+++ Day0.cs
@@ -1,19 +1,54 @@
-﻿namespace AdventOfCode2024
+﻿namespace AdventOfCode2025
 {
-    internal class Day0
+    internal class Day01
     {
         public string Part1(string inputName)
         {
             string[] RawInput = File.ReadAllLines(inputName);
 
-            return "";
+            int curNum = 50;
+            int numZero = 0;
+            foreach (string line in RawInput)
+            {
+                int dir = line[0] == 'L' ? -1 : 1;
+                int rotation = int.Parse(line[1..]);
+                curNum = (curNum + (dir * rotation) + 100) % 100;
+                if (curNum == 0)
+                {
+                    numZero++;
+                }
+            }
+
+            return numZero.ToString();
         }
 
         public string Part2(string inputName)
         {
             string[] RawInput = File.ReadAllLines(inputName);
 
-            return "";
+            int curNum = 50;
+            int numZero = 0;
+            foreach (string line in RawInput)
+            {
+                int dir = line[0] == 'L' ? -1 : 1;
+                int rotation = int.Parse(line[1..]);
+                int newVal = curNum + (dir * rotation);
+                if (newVal >= 100)
+                {
+                    numZero += newVal / 100;
+                }
+                else if (newVal <= 0)
+                {
+                    numZero += Math.Abs(newVal) / 100;
+                    if (curNum > 0)
+                    {
+                        numZero++;
+                    }
+                }
+                curNum = (newVal + 100) % 100;
+            }
+
+            return numZero.ToString();
         }
     }
 }
---
+++ Day01.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode2024
+{
+    internal class Day0
+    {
+        public string Part1(string inputName)
+        {
+            string[] RawInput = File.ReadAllLines(inputName);
+
+            return "";
+        }
+
+        public string Part2(string inputName)
+        {
+            string[] RawInput = File.ReadAllLines(inputName);
+
+            return "";
+        }
+    }
+}
